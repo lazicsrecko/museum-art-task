beb3d238981017525620499911a27f6fc09ebfef
nottebianca <sreckolazic94@gmail.com>
2021-05-25 20:24:47 +0200

    The app setup, incluing dependencies and baisc app layout

---
+++ client/src/App.js
@@ -0,0 +1,12 @@
+import "./App.css";
+
+function App() {
+  return (
+    <div className="container">
+      <div className="navigation-tree">Navigation Tree</div>
+      <div className="preview-space">Product Details</div>
+    </div>
+  );
+}
+
+export default App;
