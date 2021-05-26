08e60e86884aad9cc4cefc848fdcd3d239e8d3b5
nottebianca <sreckolazic94@gmail.com>
2021-05-26 20:15:18 +0200

    Using local storage to load and save data

--- client/src/components/main-component.js
+++ client/src/components/main-component.js
@@ -2,18 +2,24 @@ import React, { useState, useEffect, useContext } from "react";
 import { ServiceContext } from "../services/ServiceContext";
 
 function MainComponent() {
-  const [collection, setCollection] = useState({});
+  const [collectionTree, setCollectionTree] = useState({});
   const service = useContext(ServiceContext);
 
   const fetchCollection = async () => {
-    const collectionRequest = await service.mainService.getCollection();
-    setCollection(collectionRequest);
-  };
+    const localStorageCollection = JSON.parse(localStorage.getItem("collection"));
 
+    if (localStorageCollection) {
+      setCollectionTree(localStorageCollection);
+    } else {
+      const collectionRequest = await service.mainService.getCollection();
+      localStorage.setItem('collection', JSON.stringify(collectionRequest));
+      setCollectionTree(collectionRequest);
+    }
+  };
+  
   useEffect(() => {
     fetchCollection();
   }, []);
-  console.log(collection);
   return (
     <div className="container">
       <div className="navigation-tree">Navigation Tree</div>
